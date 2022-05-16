3aa3b857648c55c3e2239b28caebe7dbf7d52169
Vipin Yadav <vyvipinyadav998@gmail.com>
2022-05-16 19:12:52 +0530

    Group Anagrams

--- Arrays/GroupAnangrams.py
+++ Arrays/GroupAnangrams.py
@@ -19,6 +19,14 @@ Input: strs = ["a"]
 Output: [["a"]]
 
 '''
-
-def GroupAnagrams(strs):
-    ...
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        d=collections.defaultdict(list)
+        for s in strs:
+            count = [0]*26
+            for c in s:
+                count[ord(c)-ord('a')] =+1
+            d[tuple(count)].append(s)
+        return list(d.values())
+        
